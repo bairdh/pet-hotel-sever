8d1374765e92febeee9cf0c6c73f7eb9c20bca5f
ThyChristiansen <thyvu0411@gmail.com>
2020-06-11 13:57:24 -0500

    create table

--- database.sql
+++ database.sql
@@ -1,15 +1,16 @@
 CREATE DATABASE pet_hotel
 
-CREATE TABLE pet(
-    id
-    name
-    breed
-    color
-    checkedIn
-    ownerId
-)
 
-CREATE TABLE owner(
-    id
-    name
-)
+CREATE TABLE owner (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60),
+);
+
+CREATE TABLE pet (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60),
+    breed VARCHAR(80),
+    color VARCHAR(80),
+    checked_in VARCHAR(80)
+    owner_id integer REFERENCES owner
+);
