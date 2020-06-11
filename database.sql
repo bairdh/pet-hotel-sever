f7ed2634b4a287d115db9cfac78805252ad95715
ThyChristiansen <thyvu0411@gmail.com>
2020-06-11 14:00:10 -0500

    add , in table

--- database.sql
+++ database.sql
@@ -11,6 +11,6 @@ CREATE TABLE pet (
     name VARCHAR(60),
     breed VARCHAR(80),
     color VARCHAR(80),
-    checked_in VARCHAR(80)
+    checked_in VARCHAR(80),
     owner_id integer REFERENCES owner
 );
