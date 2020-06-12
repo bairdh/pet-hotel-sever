f11196a0a8028c3e8c5140a92b15a35e83c05f9c
Josh Kuss <joshua.d.kuss@gmail.com>
2020-06-12 11:19:17 -0500

    brought owner GET request into server.py

--- server.py
+++ server.py
@@ -17,5 +17,14 @@ def fetchPets():
     cur.execute("SELECT * FROM pet")
     return jsonify(cur.fetchall())
 
+#owner get request
+@app.route('/api/owner')
+def fetchOwner():
+    conn = psycopg2.connect("dbname=pet_hotel", )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute("SELECT * FROM owner")
+    return jsonify(cur.fetchall())
+
+
 if __name__ == "__main__":
     app.run(debug=True)
